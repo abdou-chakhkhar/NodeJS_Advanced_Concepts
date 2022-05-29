214be6d7a504ca3c26fb5b9837cb80fe18c8219e
abdou.chakhkhar <abdelmoula.chakhkhar@uit.ac.ma>
2022-05-29 19:05:22 +0100

    first commit

---
+++ notes.js
@@ -0,0 +1,24 @@
+// Node Event Loop => Single Threaded
+// Some of Node Framework / Std Lib => Not Single Threaded 
+
+// if Node were single threaded...
+// Thread 1 ( crypto.pdkdf2 (1s) + crypto.pdk (1s) = 2s total)
+
+
+
+// Questions
+
+// Can we use the threadpool for javascript code or can only nodeJS functions use it?
+// We can write custom JS that uses the thread pool
+
+// What functions in node std library use the threadpool
+// All 'fs' module functions. Some crypto stuff. Depends on OS
+
+// How does this threadpool stuff fit into the event loop?
+// Tasks running in the threadpool are the pendingOperations in our code example
+
+// What functions in node std library use the OS's async features?
+// Almost everything around networking for all OS's. Some other stuff is OS specific
+
+// How does this os async stuff async stuff fit into the event loop?
+// Tasks using the underlying OS are reflected in our pendingOSTasks array
---
+++ threads.js
@@ -0,0 +1,36 @@
+
+process.env.UV_THREADPOOL_SIZE = 5;
+
+const crypto = require('crypto');
+
+const start = Date.now();
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('1:', Date.now() - start);
+})
+
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('2:', Date.now() - start);
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('3:', Date.now() - start);
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('4:', Date.now() - start);
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('5:', Date.now() - start);
+})
+
+// Output
+// 2: 455
+// 3: 479
+// 4: 484
+// 1: 487
+// 5: 924
+
+
